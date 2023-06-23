effdbc2ed18378628a9e350fa021d3b4b51b56bf
CodeDrawing <845391508@qq.com>
2023-06-23 12:48:15 +0800

    finish show number

--- Hardware/WS2812B.c
+++ Hardware/WS2812B.c
@@ -2,7 +2,7 @@
 #include "TIM2.h"
 #include "DMA1.h"
 
-#define WS2812B_LED_QUANTITY	255
+#define WS2812B_LED_QUANTITY	256
 
 uint32_t WS2812B_Buf[WS2812B_LED_QUANTITY];	//0xGGRRBB
 uint16_t WS2812B_Bit[24*WS2812B_LED_QUANTITY+1];
@@ -19,7 +19,7 @@ void WS2812B_Init(void)
 
 void WS2812B_ClearBuf(void)
 {
-	uint8_t i;
+	uint16_t i;
 	for(i=0;i<WS2812B_LED_QUANTITY;i++)
 	{
 		WS2812B_Buf[i]=0x000000;
@@ -28,7 +28,7 @@ void WS2812B_ClearBuf(void)
 
 void WS2812B_SetBuf(uint32_t Color)
 {
-	uint8_t i;
+	uint16_t i;
 	for(i=0;i<WS2812B_LED_QUANTITY;i++)
 	{
 		WS2812B_Buf[i]=Color;
@@ -37,7 +37,7 @@ void WS2812B_SetBuf(uint32_t Color)
 
 void WS2812B_UpdateBuf(void)
 {
-	uint8_t i,j;
+	uint16_t i,j;
 	for(j=0;j<WS2812B_LED_QUANTITY;j++)
 	{
 		for(i=0;i<24;i++)
